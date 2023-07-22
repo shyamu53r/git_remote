92aca6dcbe2a96a30065c25fde26f85bfe042db6
Shyam Bharati <sunny8r0@gmail.com>
2023-07-22 12:02:17 +0530

    second commit

---
+++ subtraction.java
@@ -0,0 +1,12 @@
+class subtraction {
+
+    public static void main(String[] args) {
+
+        int first = 50;
+        int second = 20;
+
+        // subtraction of two numbers
+        int sum = first - second;
+        System.out.println(first + " - " + second + " = " + sum);
+    }
+}
